c067e1a8a028056fc2c45d7a43825bf5d56c466e
fukuhara-ran <reksapanduasa26@gmail.com>
2025-07-21 20:34:07 +0700

    chore: make item table earlier then product table

---
+++ database/migrations/2025_07_17_120651_create_items_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->integer('quantity')->default(0);
+            $table->integer('min_quantity')->default(1);
+            $table->text('note')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
